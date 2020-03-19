f974b52cc6d4e31e63f54f293ed07fe7a6d3ed26
Marcelo Brandalero <marcelo.brandalero@gmail.com>
2020-03-19 15:34:34 +0100

    update modelsim project files (make it work in windows and linux)

--- project_modelsim/setup.tcl
+++ project_modelsim/setup.tcl
@@ -1,9 +1,40 @@
 project new . FGPU
 
-set VHD_FILES [ls ../RTL | grep .vhd]
+set VHD_FILES [list \
+               \
+               FGPU_definitions.vhd \
+               FGPU_simulation_pkg.vhd \
+               \
+               DSP48E1.vhd \
+               mult_add_sub.vhd \
+               regFile.vhd \
+               ALU.vhd \
+               CV.vhd \
+               \
+               RTM.vhd \
+               CU_instruction_dispatcher.vhd \
+               CU_scheduler.vhd \
+               \
+               lmem.vhd \
+               rd_cache_fifo.vhd \
+               CU_mem_cntrl.vhd \
+               CU.vhd \
+               \
+               loc_indcs_generator.vhd \
+               init_alu_en_ram.vhd \
+               WG_dispatcher.vhd \
+               \
+               axi_controllers.vhd \
+               cache.vhd \
+               gmem_atomics.vhd \
+               gmem_cntrl_tag.vhd \
+               gmem_cntrl.vhd \
+               global_mem.vhd \
+               \
+               FGPU.vhd \
+               FGPU_tb.vhd]
 
 foreach f ${VHD_FILES} {
     project addfile ../RTL/${f}
 }
-project removefile ../RTL/float_units.vhd
-project calculateorder
+project compileall
